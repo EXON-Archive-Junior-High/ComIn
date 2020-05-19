0b133b127867e163edad7e5785d966b271c7403c
1-EXON <exon.indiv@gmail.com>
2020-05-19 13:07:27 +0900

    logo

--- ComIn1/Form1.Designer.cs
+++ ComIn1/Form1.Designer.cs
@@ -429,6 +429,7 @@
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
