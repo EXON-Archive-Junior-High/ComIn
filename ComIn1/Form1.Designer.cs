e6b69f75405b11f43bf14ed5abe24f2ba045321b
1-EXON <exon.indiv@gmail.com>
2020-05-21 10:29:25 +0900

    build

--- ComIn1/Form1.Designer.cs
+++ ComIn1/Form1.Designer.cs
@@ -216,7 +216,7 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(23, 398);
+            this.label5.Location = new System.Drawing.Point(23, 396);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(52, 23);
             this.label5.TabIndex = 30;
